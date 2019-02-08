766b8dd7c60cc6a7e0875908cda9cd1addabba88
dreammiired <dreammii@reddithub.com>
2019-02-08 11:52:09 +0700

    use ninja instead

--- setenv.bat
+++ setenv.bat
@@ -1,2 +1,7 @@
-set PATH=%PATH%;D:\LLVM\bin
-call "D:\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
+::set PATH=%PATH%;D:\LLVM\bin
+::call "D:\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
+
+set PATH=%PATH%;D:\LLVM-5.0.2-win32\bin
+call "D:\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+
+::clang -x c NUL -dM -E
