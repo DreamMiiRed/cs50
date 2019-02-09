762be4c3ac2b92d77c20470454c285debab0191c
dreammiired <dreammii@reddithub.com>
2019-02-09 10:22:29 +0700

    Join codewars

--- codewars.cpp
+++ codewars.cpp
@@ -1,7 +1,7 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <sstream>
+#include <sstream> //stringstream
 
 using namespace std;
 
@@ -31,11 +31,34 @@ std::vector<int> digitize(unsigned long n)
 //----------------------------------------    
 }
 
+std::string abbrevName(std::string name)
+{
+  string res[2];
+  stringstream ss(name);
+  int i = 0;
+  for(string s; ss >> s; )
+    res[i++] = toupper(s[0]);
+  return res[0] + '.' + res[1];
+
+//   std::string ret;
+//   ret.push_back(toupper(name[0]));
+//   ret.push_back('.');
+//   ret.push_back(toupper(name[name.find(' ') + 1] ));
+//   return ret;
+}
+
 int main() {
+    cout << "digitze" << endl;
     int N = 1234567890L;
     vector<int> vn = digitize(N);
     for(int& i : vn) {
         cout << i << ' ';
     }
     cout << endl;
+    cout << "-----------------" << endl;
+
+    cout << "Abbreviate a Two Word Name" << endl;
+    cout << abbrevName("John Smith") << endl;
+    cout << abbrevName("Paul young") << endl;
+    cout << "-----------------" << endl;
 }
